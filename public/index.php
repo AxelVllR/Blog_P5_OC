f8299c8a8e12f26fd8f60adc7dcbd4b8268d8299
AxelVllR <vallieraxel@gmail.com>
2021-04-20 10:40:42 +0200

    change links

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ session_start();
 ini_set('display_errors', 1);
 require '../vendor/autoload.php';
 $request = $_SERVER['REQUEST_URI'];
+var_dump(get_current_user());
 $exp = explode("?", $request);
 //var_dump($request);
 $app = new \App\App;
-
 
 // echo $twig->render('test.html.twig');
 
